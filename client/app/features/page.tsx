d53ecf241767d3d5a8596d4e5649de27c2e99252
diyajojo <diyaj2122@gmail.com>
2025-02-01 20:09:47 +0530

    second feature

--- client/app/features/page.tsx
+++ client/app/features/page.tsx
@@ -9,7 +9,8 @@ import {
   CardDescription,
   CardContent,
   CardFooter,
-} from "../components/ui/card";
+} from "./../components/ui/card";
+
 
 const Features = () => {
   const router = useRouter();
